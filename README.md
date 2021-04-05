40ec4a723c51e4000c9ea6fc7b13d43412fca098
RussellSB <r.sammutbonnici@gmail.com>
2021-04-05 15:22:53 +0100

    Cleaning root directory, documenting readme

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # audio-identification
-Music information retrieval system with audio fingerprints.
+A musical audio retrieval system with fingerprints, following the algorithm presented by [Wang 2003](https://www.researchgate.net/publication/220723446_An_Industrial_Strength_Audio_Search_Algorithm). This is the approach that Shazam took at the time, but has improved on since then.
+
+## Setup
+The required packages are enlisted in requirements.txt. It's recommended to use a virtualenv using pip (alternatively conda should also suffice).  From a command line interface with pip, the virtual environment can be created, activated and updated with the required packages as follows:
+
+```
+virtualenv -p python3 .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+```
+
+## Data
+
+For the data I/O you a folder named "data" must be present from the root directory. The following directories may also need to be included.
+
+```
+data
+├── database_recordings
+├── fingerprints
+├── output
+├── query_recordings
+└── results
+```
+
+### Input
+
+Directories for input are:
+
+* database_recordings - This has audio files from a subset of the GTZAN dataset. May be downloaded from [here](https://collect.qmul.ac.uk/down?t=R8SDLMOKUOSCD2VB/6P63FFT4AN0581R7V49FJKO).
+* query_recordings - This has some audio files from the same subset, but cropped and with noise added to it for a fairly challenging evaluation. May be downloaded from [here](https://collect.qmul.ac.uk/down?t=450TPH3RDUJNA920/6P4TNTJT7GSTR7NUC226IJ8).
+
+The database set is sampled at 22050Hz, whereas the query set at 44100 Hz. Nonetheless, in this approach they are both sampled at 22050 Hz.
+
+### Output
+
+Directories for output are:
+
+* fingerprints - Stores arrays of audio fingerprints in .pickle format. Each file would correspond to a version of the fingerprint database (with specific parameters)
+* output - Stores .txt files with a line per query file. The first word corresponds to the query file, and the following three word correspond to the retrieved match in descending order of similarity. Only the top three are returned for later evaluating up to a rank of three.
+* results - Stores .txt files with recall results from tested experiments. This is not automatically outputted to or required. But for transparency purposes, console logged results from the evaluation have been copied and formatted into text files here.
+
+Files from the fingerprint folder were not included due to large file sizes - but output and results files are included as they only store text files with small file sizes.
+
+## Usage
+
+### Main
+
+An example use case of the system may be ran from the main file, as enlisted below. With a call to three different methods, it indexes, queries, and evaluates all the dataset files in that order. The final evaluation results are printed to the command line.
+
+```
+python main.py
+```
+
+### Experiments
+
+To replicate the performed experiments, you may simply run experiments.py. Here a set of experiments per parameters were tested to investigate the affectiveness of different approaches to indexing. 
+
+```
+python experiments.py
+```
--- main.py
+++ main.py
@@ -3,62 +3,13 @@ from match import audioIdentification
 from evaluate import evaluate
 
 
-# A wide variety of example cases, performed in order to later evaluate parameters for each step of the pipeline
-def index_experiments():
+# Runs final evaluation, indexing from best parameters deduced from experiments
+def run_final():
+    fingerprintBuilder('data/database_recordings', 'data/fingerprints/final.pickle', spectype='stft', shape='disk', uniform=True, gap=0, targetsize=(400,200))
+    audioIdentification('data/query_recordings', 'data/fingerprints/final.pickle', 'data/output/final.txt')
+    evaluate('data/output/final.txt', 'avg')
 
-    # Investigating spectrogram type
-    fingerprintBuilder('data/database_recordings', 'data/fingerprints/stft.pickle', spectype='stft')
-    fingerprintBuilder('data/database_recordings', 'data/fingerprints/mel.pickle', spectype='mel')
-    fingerprintBuilder('data/database_recordings', 'data/fingerprints/cqt.pickle', spectype='cqt')
-
-    # Investigating shape (const map)
-    fingerprintBuilder('data/database_recordings', 'data/fingerprints/disk.pickle', shape='disk')
-    fingerprintBuilder('data/database_recordings', 'data/fingerprints/diamond.pickle', shape='diamond')
-    fingerprintBuilder('data/database_recordings', 'data/fingerprints/square.pickle', shape='square')
-    # Investigating uniformity (const map)
-    fingerprintBuilder('data/database_recordings', 'data/fingerprints/uniform-true.pickle', uniform=True)
-    fingerprintBuilder('data/database_recordings', 'data/fingerprints/uniform-false.pickle', uniform=False)
-    # Investigating neighbourhood (const map)
-    fingerprintBuilder('data/database_recordings', 'data/fingerprints/neigh-5.pickle', neighbourhood=5)
-    fingerprintBuilder('data/database_recordings', 'data/fingerprints/neigh-10.pickle', neighbourhood=10)
-    fingerprintBuilder('data/database_recordings', 'data/fingerprints/neigh-20.pickle', neighbourhood=20)
-
-    # Investigating targetsize (hashing)
-    fingerprintBuilder('data/database_recordings', 'data/fingerprints/gap-0.pickle', gap=0)
-    fingerprintBuilder('data/database_recordings', 'data/fingerprints/gap-50.pickle', gap=50)
-    fingerprintBuilder('data/database_recordings', 'data/fingerprints/gap-100.pickle', gap=100)
-    # Investigating targetsize (hashing)
-    fingerprintBuilder('data/database_recordings', 'data/fingerprints/t-200-400.pickle', targetsize=(200,400))
-    fingerprintBuilder('data/database_recordings', 'data/fingerprints/t-200-200.pickle', targetsize=(200,200))
-    fingerprintBuilder('data/database_recordings', 'data/fingerprints/t-400-200.pickle', targetsize=(400,200))
-
-
-# Use data list as we dont have to worry about parameters, but just experiment names
-def match_experiments(data):
-    for d in data:
-        audioIdentification('data/query_recordings', 'data/fingerprints/'+d+'.pickle', 'data/output/'+d+'.txt')
-
-
-# Use data list as we dont have to worry about parameters, but just experiment names
-def evaluate_experiments(data, scope):
-    for d in data:
-        evaluate('data/output/'+d+'.txt', scope)
-
-
-# Made for reproducibility purposes, this should run all tested experiments with varying parameters
-def run_experiments():
-
-    data = ['stft', 'mel', 'cqt',
-        'disk', 'diamond', 'square',
-        'uniform-true', 'uniform-false',
-        'neigh-5', 'neigh-10', 'neigh-20',
-        'gap-0', 'gap-50', 'gap-100',
-        't-200-400', 't-200-200', 't-400-200']
-
-    #index_experiments()  # Assemble fingerprints of database
-    #match_experiments(data)  # Assemble fingerprints of queries, and match with that of database
-    evaluate_experiments(data, 'classical')  # Evaluate matching results via recall up to three ranks
-    
 
 if __name__ == '__main__':
-    run_experiments()
+    # run_experiments()
+    run_final()
