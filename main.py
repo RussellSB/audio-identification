20648faadb202404f632ee398831bb8fcdc20a77
RussellSB <r.sammutbonnici@gmail.com>
2021-04-05 17:08:29 +0100

    Refactoring

--- main.py
+++ main.py
@@ -5,11 +5,9 @@ from evaluate import evaluate
 
 # Runs final evaluation, indexing from best parameters deduced from experiments
 def run_final():
-    fingerprintBuilder('data/database_recordings', 'data/fingerprints/final.pickle', spectype='stft', shape='disk', uniform=True, gap=0, targetsize=(400,200))
+    fingerprintBuilder('data/database_recordings', 'data/fingerprints/final.pickle', spectype='stft', shape='disk', uniform=False, gap=0, targetsize=(400,200))
     audioIdentification('data/query_recordings', 'data/fingerprints/final.pickle', 'data/output/final.txt')
     evaluate('data/output/final.txt', 'avg')
 
-
 if __name__ == '__main__':
-    # run_experiments()
     run_final()
